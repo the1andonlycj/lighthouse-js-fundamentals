8adc93d878283c922e57d1491084787f2fd48ed7
Charlie Hayes <cjhayestutor@gmail.com>
2020-05-03 18:54:55 +0000

    Added plentyOfDevs exercise

---
+++ plentyOfDevs.js
@@ -0,0 +1,5 @@
+const ageCalculator = function(name, yearOfBirth, currentYear) {
+  let age = currentYear - yearOfBirth;
+  return(name + " is " + age + " years old.");
+};
+console.log(ageCalculator ("Miranda", 1983, 2020));
